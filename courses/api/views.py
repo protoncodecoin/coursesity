3af4d09252130705015e6f306096f5e5aa383c3b
Prince Asante <princeaffumasante@gmail.com>
2024-07-17 03:16:53 -0100

    add restAPI to expose resource add api endpoints for subject and courses

---
+++ courses/api/views.py
@@ -0,0 +1,15 @@
+from django.db.models import Count
+from rest_framework import generics
+
+from courses.api.serializers import SubjectSerializer
+from courses.models import Subject
+
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
+    serializer_class = SubjectSerializer
+
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
+    serializer_class = SubjectSerializer
