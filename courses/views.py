5c7125676a97fb15cf73339e45d2cf4a103181d2
Prince Asante <princeaffumasante@gmail.com>
2024-07-25 00:06:17 +0000

    add cart feature for users to add course to cart
    add price field to course model to set price

--- courses/views.py
+++ courses/views.py
@@ -238,7 +238,7 @@ class CourseDetailView2(TemplateResponseMixin, View):
     model = Course
     template_name = "courses/course/detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["enroll_form"] = CourseEnrollForm(initial={"course": self.object})
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["enroll_form"] = CourseEnrollForm(initial={"course": self.object})
+    #     return context
